d5d75afc92be3a57e9b76d647ff662af0546c540
palashjohari <sanchit2411@gmail.com>
2017-01-17 19:27:08 +0530

    footers links and client n partner logos

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
 /**
  * Created by neerajsingh on 16/12/16.
  */
+
+<script>
+$( "div#team1" ).addClass(function( index, currentClass ) {
+  var addedClass;
+ 
+  if ( currentClass === "red" ) {
+    addedClass = "green";
+    $( "p" ).text( "There is one green div" );
+  }
+ 
+  return addedClass;
+});
+</script>
